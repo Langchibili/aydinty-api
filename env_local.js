ee8747d09058f45d3cc21de1b7098fcd1418da60
langchibili <langchibili@gmail.com>
2022-01-19 20:41:21 +0200

    adding environmental variables config

---
+++ env_local.js
@@ -0,0 +1,6 @@
+module.exports= {
+    set: function(){
+        process.env.PORT = 6000
+        process.env.NODE_ENV = "local"
+    }
+}
---
+++ env_production.js
@@ -0,0 +1,6 @@
+module.exports= {
+    set: function(){
+        process.env.PORT = 5000
+        process.env.NODE_ENV = "production"
+    }
+}
